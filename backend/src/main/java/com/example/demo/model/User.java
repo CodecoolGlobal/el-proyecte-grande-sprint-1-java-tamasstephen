86010b99a2e2110b78c27c4dece08d557e401220
matekaszas13 <matekaszas17@gmail.com>
2022-03-16 14:38:29 +0100

    Feat: Add user class and its basic structure

--- backend/src/main/java/com/example/demo/model/User.java
+++ backend/src/main/java/com/example/demo/model/User.java
@@ -1,12 +1,31 @@
 package com.example.demo.model;
 
+import java.util.Objects;
+
 public class User {
     private long id;
 //    private Content userContent;
-    private String name;
+    private String userName;
     private String email;
     private String password;
     private String profileImage;
 
+    // TODO: 16/03/2022 Add constructor after merge
+
+    public boolean isSameCategory(Category category){
+        return true;
+    }
+
+    public String getEndpoint(){
+        return "";
+    }
+
+    public boolean isUserHasSameName(String name){
+        return Objects.equals(userName, name);
+    }
+
+    public void payUser(){
+
+    }
 
 }
