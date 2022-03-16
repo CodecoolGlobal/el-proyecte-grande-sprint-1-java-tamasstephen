ae682b6a45c45735a777fa0cdf0d9090c2fef7ea
tamasstephen <kicsitnagyon@gmail.com>
2022-03-16 14:20:25 +0100

    chore: Add Category enum

--- backend/src/main/java/com/example/demo/CreatorSupporter.java
+++ backend/src/main/java/com/example/demo/CreatorSupporter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CreatorSupporter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CreatorSupporter.class, args);
 	}
 
 }
