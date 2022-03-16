e99b82c80ada0ff94ccabcf7016d6a033b4c99e6
matekaszas13 <matekaszas17@gmail.com>
2022-03-16 14:33:57 +0100

    Feat: Add user class and its fields

---
+++ backend/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class User {
+    private long id;
+//    private Content userContent;
+    private String name;
+    private String email;
+    private String password;
+    private String profileImage;
+
+
+}
