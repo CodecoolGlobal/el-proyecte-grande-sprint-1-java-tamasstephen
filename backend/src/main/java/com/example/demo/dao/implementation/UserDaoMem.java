218f8e04cc9032fd11d53a3356ce6ca2b2e77de4
tamasstephen <kicsitnagyon@gmail.com>
2022-03-16 21:01:56 +0100

    refactor: Remove unnecessary UserDao methods

--- backend/src/main/java/com/example/demo/dao/implementation/UserDaoMem.java
+++ backend/src/main/java/com/example/demo/dao/implementation/UserDaoMem.java
@@ -16,8 +16,8 @@ public class UserDaoMem implements UserDao {
     private static final ArrayList<User> users = new ArrayList<>();
 
     @Override
-    public List<User> get(String name) {
-        return users.stream().filter(user -> user.getUserName().equals(name)).collect(Collectors.toList());
+    public List<User> get(String email) {
+        return users.stream().filter(user -> user.getEmail().equals(email)).collect(Collectors.toList());
     }
 
     @Override
@@ -25,15 +25,6 @@ public class UserDaoMem implements UserDao {
         return users.stream().filter(user -> user.hasSameId(id)).findFirst();
     }
 
-    @Override
-    public Optional<User> getUserByPageLink(String pageLink) {
-        return users.stream().filter(user -> user.hasSameEndpoint(pageLink)).findFirst();
-    }
-
-    @Override
-    public List<User> getUsersByCategory(Category category) {
-        return users.stream().filter(user -> user.isSameCategory(category)).collect(Collectors.toList());
-    }
 
     @Override
     public void add(User user) {
