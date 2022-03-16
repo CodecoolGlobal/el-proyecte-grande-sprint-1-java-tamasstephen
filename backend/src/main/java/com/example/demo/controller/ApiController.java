b2b919774095d8f184185755122659aa24d78c25
tamasstephen <kicsitnagyon@gmail.com>
2022-03-16 18:17:42 +0100

    chore: Add content getter api

--- backend/src/main/java/com/example/demo/controller/ApiController.java
+++ backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.user.Content;
 import com.example.demo.model.user.User;
+import com.example.demo.service.ContentService;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +20,14 @@ public class ApiController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ContentService contentService;
+
     @PostMapping("/add-creator")
     public void add(@RequestBody User user, HttpSession session){
         long newId = userService.getAllUsers() == null ? 1 : userService.getAllUsers().size() + 1;
         user.setId(newId);
         userService.add(user);
-        System.out.println(user.getEmail());
-        System.out.println(userService.getAllUsers().size());
         session.setAttribute("userId", newId);
 
     }
@@ -33,21 +35,19 @@ public class ApiController {
     @PostMapping("/add-content")
     public boolean add(@RequestBody Content content, HttpSession session){
         Long userId = (Long) session.getAttribute("userId");
-        if (userId == null){
+        if (userId == null || userService.getUser(userId).isEmpty()){
             return false;
-        }
-        Optional<User> user = userService.getUser(userId);
-        if (user.isEmpty()){
-            return false;
-        }  else {
+        } else {
+            User user = userService.getUser(userId).get();
             content.setUserId(userId);
-            user.get().setContent(content);
+            user.setContent(content);
+            contentService.add(content);
             return true;
         }
     }
 
     @GetMapping("/all-creators")
-    public List<User> getAll(){
-        return userService.getAllUsers();
+    public List<Content> getAll(){
+        return contentService.getAll();
     }
 }
