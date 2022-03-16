a1a3d43dd509a2e2ea61f04c5ea0081e392c0dd4
matekaszas13 <matekaszas17@gmail.com>
2022-03-16 14:40:17 +0100

    Feat: Add isSameCategory method

--- backend/src/main/java/com/example/demo/model/User.java
+++ backend/src/main/java/com/example/demo/model/User.java
@@ -4,7 +4,7 @@ import java.util.Objects;
 
 public class User {
     private long id;
-//    private Content userContent;
+    private Content userContent;
     private String userName;
     private String email;
     private String password;
@@ -13,7 +13,7 @@ public class User {
     // TODO: 16/03/2022 Add constructor after merge
 
     public boolean isSameCategory(Category category){
-        return true;
+        return userContent.getCategory() == category;
     }
 
     public String getEndpoint(){
