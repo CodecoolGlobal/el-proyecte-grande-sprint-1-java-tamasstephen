f1e51a59c2c056fd376fd57181e0ce026c4ae20a
tamasstephen <kicsitnagyon@gmail.com>
2022-03-16 17:48:48 +0100

    chore: Add ContentDao interface

---
+++ backend/src/main/java/com/example/demo/dao/ContentDao.java
@@ -0,0 +1,23 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.user.Category;
+import com.example.demo.model.user.Content;
+import com.example.demo.model.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContentDao {
+
+    List<Content> get(String creatorName);
+
+    Optional<Content> get(long userId);
+
+    Optional<Content> getCreatorPageByPageLink(String pageLink);
+
+    List<Content> getContentsByCategory(Category category);
+
+    void add(Content content);
+
+    List<Content> getAll();
+}
