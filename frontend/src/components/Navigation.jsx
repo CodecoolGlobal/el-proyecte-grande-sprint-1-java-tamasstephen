9a5b33ee3c624e393229693c0dc14f9bf44d8f56
tamasstephen <kicsitnagyon@gmail.com>
2022-03-26 16:25:20 +0100

    style: Add hamburger icon

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { HiMenu } from "react-icons/hi";
 
 export const Navigation = () => {
   return (
-    <nav className="flex justify-center">
-      <div className="p-4 flex justify-between container mx-xl xl:px-[5rem]">
+    <nav className="flex justify-center ">
+      <div className="p-4 flex justify-between container mx-xl xl:px-[5rem] content-center">
         <div className="logo">
           <a href="/">DropCoin</a>
         </div>
@@ -12,7 +13,9 @@ export const Navigation = () => {
           <li className="pl-4">Login</li>
           <li className="pl-4">Sign Up</li>
         </ul>
-        <div className="hamburger md:hidden">close</div>
+        <div className="flex items-center hamburger md:hidden">
+          <HiMenu />
+        </div>
       </div>
     </nav>
   );
