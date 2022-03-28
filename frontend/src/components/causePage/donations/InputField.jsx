c6560a79abb2c7522d2a922f2400bf12980d525d
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 19:53:23 +0200

    Feat:Add cleaner structure and remove key variable

---
+++ frontend/src/components/causePage/donations/InputField.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const InputField = ({ setState, placeholder }) => {
+  function handleChange(event) {
+    console.log(event.currentTarget.value);
+    setState(event.currentTarget.value);
+  }
+
+  return (
+    <input type="text" onChange={handleChange} placeholder={placeholder} />
+  );
+};
+
+export default InputField;
