12d92d30404a61f19e1a52f62670e9b0713060f8
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 19:54:03 +0200

    Feat: Add make comment&name only string

--- frontend/src/components/causePage/donations/DonationForm.jsx
+++ frontend/src/components/causePage/donations/DonationForm.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import CoinAmount from "./CoinAmount";
 import SubHeadline from "../SubHeadline";
+import InputField from "./InputField";
 
 const DonationForm = () => {
 
-  const [name, setName] = useState({name : ''});
+  const [name, setName] = useState('');
 
-  const [comment, setComment] = useState({comment : ''});
+  const [comment, setComment] = useState('');
 
   return (
     <>
@@ -16,6 +17,8 @@ const DonationForm = () => {
         <CoinAmount amount={5} />
         <CoinAmount amount={10} />
       </div>
+      <InputField setstate={setName} placeholder={'Add your name'}/>
+      <InputField setstate={setComment} placeholder={'Add a comment if you want'}/>
     </>
   );
 };
