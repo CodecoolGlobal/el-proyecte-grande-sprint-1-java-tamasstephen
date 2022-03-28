094fda1d051b0748238760b0271acd5ef341d217
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 17:36:36 +0200

    Feat: Add Donation component basic structure

---
+++ frontend/src/components/causePage/Donation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CoinAmount from "./CoinAmount";
+
+const Donation = () => {
+  return (
+    <>
+      <CoinAmount amount={2} />
+    </>
+  );
+};
+
+export default Donation;
