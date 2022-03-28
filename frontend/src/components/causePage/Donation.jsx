f09f6c0c4d09676410bdc32c7897e5a287def339
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 17:43:25 +0200

    Refactor: Change HeadLine to SubHeadLine

--- frontend/src/components/causePage/Donation.jsx
+++ frontend/src/components/causePage/Donation.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CoinAmount from "./CoinAmount";
+import SubHeadline from "./SubHeadline";
 
 const Donation = () => {
   return (
     <>
+    <SubHeadline name={"Drop Those Coins"}/>
       <CoinAmount amount={2} />
     </>
   );
