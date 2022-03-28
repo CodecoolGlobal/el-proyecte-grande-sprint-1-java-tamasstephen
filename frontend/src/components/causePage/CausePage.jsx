bc286d6b8954ad6f2dd5898472e006aed318e171
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 16:32:42 +0200

    Feat: Add basic CausePage structure,headline,image

---
+++ frontend/src/components/causePage/CausePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Headline from '../Headline'
+import Image from './Image'
+
+const CausePage = () => {
+  return (
+    <>
+    {/* <Navigation/> */}
+    <Headline isTitle={true} title={"Help Konoha"}/>
+    <Image/>
+    </>
+  )
+}
+
+export default CausePage
