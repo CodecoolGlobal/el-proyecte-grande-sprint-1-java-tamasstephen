679408ca474a708ea3e42f510e8c8038b10ff1a1
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 16:11:33 +0200

    Feat: Add route to get cause page

--- frontend/src/controller/Router.jsx
+++ frontend/src/controller/Router.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "../components/landing/Landing";
 import App from "../components/App";
+import CausePage from "../components/causePage/CausePage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Landing />} />
+        <Route path="create-cause" element={<CausePage/>}/>
       </Route>
     </Routes>
   );
