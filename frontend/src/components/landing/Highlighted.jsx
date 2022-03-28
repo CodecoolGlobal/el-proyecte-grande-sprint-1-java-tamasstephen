c5cc17e4d93ba7be1aa8f4b7952742a59c4eb4c7
tamasstephen <kicsitnagyon@gmail.com>
2022-03-28 14:37:10 +0200

    refactor: Extract image fetching into utils

--- frontend/src/components/landing/Highlighted.jsx
+++ frontend/src/components/landing/Highlighted.jsx
@@ -3,6 +3,7 @@ import Cause from "../model/Cause";
 import Headline from "../Headline";
 import { shortenDescription } from "../../utils/util.js";
 import { dataHandler } from "../../data/dataHandler";
+import { fetchImages } from "../../utils/util.js";
 
 const Highlighted = () => {
   const [causes, setCause] = useState([]);
@@ -12,25 +13,12 @@ const Highlighted = () => {
       return await dataHandler.getHighlights();
     };
 
-    const fetchImages = async (listOfCauses) => {
-      const newCauses = [];
-      for (const cause of listOfCauses) {
-        const cardImageBlob = await dataHandler.getImageForCard(cause.pageLink);
-        const imageUrl = URL.createObjectURL(cardImageBlob);
-        const myCause = { ...cause };
-        myCause["imageLink"] = imageUrl;
-        newCauses.push(myCause);
-      }
-      console.log(newCauses);
-      setCause(() => [...newCauses]);
-    };
-
-    fetchCauses().then((data) => {
+    fetchCauses().then((data, setCause) => {
       const myData = [...data];
       myData.forEach((data) => {
         data.description = shortenDescription(data.description);
       });
-      fetchImages(myData);
+      fetchImages(myData, setCause);
     });
   }, []);
 
