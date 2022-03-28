56b2f33247300bcc102d95e4c114aa376e3796c9
matekaszas13 <matekaszas17@gmail.com>
2022-03-28 17:04:15 +0200

    Feat: Add Description component

---
+++ frontend/src/components/causePage/Description.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <p>
+      Bacon ipsum dolor amet chuck shank shankle, tenderloin spare ribs doner
+      burgdoggen. Strip steak porchetta brisket, chuck tail doner cupim. Cupim
+      corned beef ball tip pork chop meatball sirloin. Jerky andouille rump
+      bresaola turkey shank, flank ball tip strip steak venison meatball. Cupim
+      kevin leberkas landjaeger flank bacon. Ground round alcatra brisket ham
+      ball tip jerky meatloaf, salami picanha. Meatball prosciutto turducken,
+      beef short loin venison kevin andouille tri-tip chuck alcatra spare ribs
+      jowl pancetta shank.
+    </p>
+  );
+};
+
+export default Description;
