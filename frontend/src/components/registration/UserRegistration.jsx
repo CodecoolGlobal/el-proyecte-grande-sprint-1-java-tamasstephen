b7e9152dd25562df853e052660e4b81732436003
tamasstephen <kicsitnagyon@gmail.com>
2022-03-29 18:02:48 +0200

    style: Add user reg form styles

--- frontend/src/components/registration/UserRegistration.jsx
+++ frontend/src/components/registration/UserRegistration.jsx
@@ -2,12 +2,12 @@ import React, { useRef, useState } from "react";
 import { dataHandler } from "../../data/dataHandler";
 import StringInput from "./StringInput";
 import { useNavigate } from "react-router-dom";
+import Error from "./Error";
+import Headline from "../Headline";
+import Submit from "./Submit";
 
 const UserRegistration = ({ setLoginState }) => {
   let navigate = useNavigate();
-  const errorContainer = useRef(null);
-  const [errorContainerState, changeErrorState] = useState("hidden");
-  const [errorMessage, changeMessage] = useState("");
   const [emailState, changeEmailValue] = useState({
     name: "email",
     type: "email",
@@ -20,6 +20,11 @@ const UserRegistration = ({ setLoginState }) => {
     type: "password",
     label: "Password:",
     placeholder: "",
+  });
+  const [errorState, setError] = useState({
+    boxState: "hidden",
+    text: "",
+    textState: "invisible",
   });
 
   async function registerUser(event) {
@@ -36,22 +41,27 @@ const UserRegistration = ({ setLoginState }) => {
       setLoginState({ logout: "", login: "hidden" });
       navigate("/cause-registration");
     } else {
-      changeMessage(result.message);
-      changeErrorState("");
+      setError({ boxState: "", text: result.message, textState: "" });
     }
   }
 
   return (
-    <form onSubmit={registerUser}>
-      <div>
-        <p ref={errorContainer} className={errorContainerState}>
-          {errorMessage}
-        </p>
-      </div>
-      <StringInput inputProps={emailState} />
-      <StringInput inputProps={passwordState} />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="flex flex-col items-center pt-16">
+      <form
+        onSubmit={registerUser}
+        className="px-4 md:w-1/2 max-w-xl flex flex-col justify-center"
+      >
+        <Headline isTitle={true} title="Register" />
+        <div className={errorState.boxState + " pt-4 md:pt-8"}>
+          <Error errorState={errorState} />
+        </div>
+        <div className="pt-4 md:pt-8 pb-4 md:pb-8">
+          <StringInput inputProps={emailState} />
+          <StringInput inputProps={passwordState} />
+        </div>
+        <Submit title="Submit" />
+      </form>
+    </div>
   );
 };
 
