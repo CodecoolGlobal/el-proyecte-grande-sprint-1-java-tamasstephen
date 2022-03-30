20dda5eca98e0164370e27cd936159d2338f1660
tamasstephen <kicsitnagyon@gmail.com>
2022-03-30 20:46:24 +0200

    style: Adjust card size to breakpoints

--- frontend/src/components/model/Cause.jsx
+++ frontend/src/components/model/Cause.jsx
@@ -3,6 +3,7 @@ import CardTitle from "../CardTitle";
 import Tag from "../Tag";
 
 const Cause = ({ cause }) => {
+  console.log(cause);
   const category = cause?.category ? cause.category : "general";
   return (
     <div className="rounded-lg bg-white overflow-hidden shadow-lg shadow-indigo-500/30 hover:shadow-2xl hover:shadow-orange-500/30 hover:-translate-y-1 transition-all ease-in duration-200">
