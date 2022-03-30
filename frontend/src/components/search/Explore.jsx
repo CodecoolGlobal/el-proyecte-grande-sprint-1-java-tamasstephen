72b5f0defd46b20f701725a89ecf3deb9e375fea
tamasstephen <kicsitnagyon@gmail.com>
2022-03-30 20:01:21 +0200

    chore: Add headline

--- frontend/src/components/search/Explore.jsx
+++ frontend/src/components/search/Explore.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Cause from "../model/Cause";
+import Headline from "../Headline";
 
 const Explore = ({ causes }) => {
   return (
-    <div>
-      <div>
-        {causes.map((cause) => (
-          <Cause key={parseInt(cause.userId)} cause={cause} />
-        ))}
+    <div className="px-4 flex justify-center">
+      <div className="max-w-7xl pt-8 md:pt-16">
+        <Headline isTitle={true} title="Explore" />
+        <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:container pt-4 md:pt-16">
+          {causes.map((cause) => (
+            <Cause key={parseInt(cause.userId)} cause={cause} />
+          ))}
+        </div>
       </div>
     </div>
   );
