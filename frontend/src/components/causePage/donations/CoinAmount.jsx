3322cded1e63a61c07d84d115f8ca6b3b4c03af9
matekaszas13 <matekaszas17@gmail.com>
2022-03-30 10:43:27 +0200

    Style: Add basic style to CoinAmount

--- frontend/src/components/causePage/donations/CoinAmount.jsx
+++ frontend/src/components/causePage/donations/CoinAmount.jsx
@@ -4,8 +4,11 @@ const CoinAmount = ({ amount, setCoinAmount }) => {
   const coinText = amount === 1 ? "coin" : "coins";
 
   return (
-    <button onClick={() => setCoinAmount(amount)}>
-      {amount} {coinText}
+    <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mr-1 ml-2" onClick={() => setCoinAmount(amount)}>
+      <div className="flex flex-row">
+        {amount} <p className="">{coinText}</p>
+      </div>
+      
     </button>
   );
 };
