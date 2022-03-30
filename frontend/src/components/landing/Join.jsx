0075e9d75fc793b23271974353c88d43bf549206
tamasstephen <kicsitnagyon@gmail.com>
2022-03-30 20:46:08 +0200

    style: Fix box size

--- frontend/src/components/landing/Join.jsx
+++ frontend/src/components/landing/Join.jsx
@@ -5,7 +5,7 @@ import Button from "../Button";
 const Join = () => {
   return (
     <div className="py-4 md:p-0 pb-20 md:pb-40 flex justify-center  px-4 md:px-0">
-      <div className="flex flex-col justify-center items-center bg-slate-900 md:w-8/12 2xl:container  rounded-lg py-4 md:py-12">
+      <div className="flex flex-col justify-center items-center bg-slate-900 md:w-8/12 2xl:max-w-screen-lg rounded-lg py-4 md:py-12 shadow-lg shadow-orange-400/30">
         <div className="w-10/12, md:w-1/2 flex flex-col justify-center items-center px-4 gap-4 md:gap-8">
           <Headline
             isTitle={false}
