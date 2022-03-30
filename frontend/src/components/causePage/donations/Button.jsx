34665781e4df54bc276095c7c0542d754ac0dd59
matekaszas13 <matekaszas17@gmail.com>
2022-03-30 10:43:08 +0200

    Style: Add basic style to button

--- frontend/src/components/causePage/donations/Button.jsx
+++ frontend/src/components/causePage/donations/Button.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({moneyAmount}) => {
+const Button = ({ moneyAmount }) => {
   return (
-    <button type='submit'>Pay ${moneyAmount}</button>
-  )
-}
+    <button
+      className="w-11/12 mb-2 mt-2 bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-1 ml-2"
+      type="submit"
+    > <p className="">
+      Pay ${moneyAmount}
+    </p>
+      
+    </button>
+  );
+};
 
-export default Button
+export default Button;
