9af6e8799ba7c26097877939e1e6c90a339a1e3b
tamasstephen <kicsitnagyon@gmail.com>
2022-03-30 20:45:22 +0200

    fix: Fix headline color

--- frontend/src/components/Headline.jsx
+++ frontend/src/components/Headline.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Headline = ({ isTitle, title, isLight }) => {
-  const color = isLight ? "text-white" : "text-indigo-900";
+  const color = isLight ? "text-slate-100 " : "text-indigo-900";
   const classes =
-    "text-3xl w-8/12 md:w-max md:text-4xl lg:text-5xl text-indigo-900 font-bold tracking-tighter " +
+    "text-3xl w-8/12 md:w-max md:text-4xl lg:text-5xl text-indigo-900 font-bold tracking-tighter xl:max-w-screen-lg " +
     color;
   return isTitle ? (
     <h1 className={classes}>{title}</h1>
