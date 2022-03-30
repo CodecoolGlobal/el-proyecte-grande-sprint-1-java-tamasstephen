e2896c982429613dcfcfaae5eaa013e8dd3a7681
tamasstephen <kicsitnagyon@gmail.com>
2022-03-30 18:41:02 +0200

    chore: Add wrapper container for search

---
+++ frontend/src/components/search/ExploreContainer.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { dataHandler } from "../../data/dataHandler";
+
+const ExploreContainer = () => {
+  const { searchParameter } = useParams();
+  console.log(searchParameter);
+
+  useEffect(() => {
+    const getCauses = async (param = "") => {
+      if (searchParameter) {
+      } else {
+        return await dataHandler.getAllCauses();
+      }
+    };
+    getCauses().then((data) => console.log(data));
+  }, []);
+
+  return <div>ExploreContainer</div>;
+};
+
+export default ExploreContainer;
