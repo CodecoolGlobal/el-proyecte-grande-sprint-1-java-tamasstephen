184ffa011e74d0400f0b3c807bf0f214ccf5cb6f
matekaszas13 <matekaszas17@gmail.com>
2022-04-13 19:30:24 +0200

    Feat: Add properties to edit/update button

--- frontend/src/components/ButtonMid.jsx
+++ frontend/src/components/ButtonMid.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-const ButtonMid = ({ buttonState, callBack, secondary }) => {
+const ButtonMid = ({ buttonState, callBack, secondary, updateFormFUnction, formVisibility, changeButtonState }) => {
   const style = secondary
     ? "bg-orange-50 border-2 border-indigo-400 shadow-md shadow-indgo-400/30 text-indigo-700"
     : "bg-indigo-700 shadow-md shadow-indigo-400/30 text-white";
 
+  const test = () => {}
   return (
     <button
       className={"py-1 px-6 font-medium rounded-lg " + style}
-      onClick={callBack}
+      onClick={secondary == false ? test() : () => callBack(updateFormFUnction, formVisibility, buttonState, changeButtonState)}
       type={buttonState.type}
     >
       {buttonState.label}
