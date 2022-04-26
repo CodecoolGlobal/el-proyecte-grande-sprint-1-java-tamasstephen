f49cfb4deff4cf28ff78eb44d05b8d942d50d64b
matekaszas13 <matekaszas17@gmail.com>
2022-04-26 14:25:09 +0200

    Feat: Add filters and beans

--- backend/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.security;
 
+import com.example.demo.security.jwt.AuthEntryPointJwt;
+import com.example.demo.security.jwt.AuthTokenFilter;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +13,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,15 +24,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
+    private final AuthEntryPointJwt unAuthorizedHandler;
 
     @Autowired
-    public ApplicationSecurityConfig(UserService userService){
+    public ApplicationSecurityConfig(UserService userService, AuthEntryPointJwt authEntryPointJwt){
         this.userService = userService;
+        this.unAuthorizedHandler = authEntryPointJwt;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unAuthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/highlighted"
                         , "/creator/support",
@@ -39,8 +49,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/user",
                         "/login",
                         "/creator-profile").permitAll()
+                .antMatchers("/creator-profile-set").hasAnyRole("CUSTOMER")
                 .anyRequest()
                 .authenticated();
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -66,4 +78,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
 }
