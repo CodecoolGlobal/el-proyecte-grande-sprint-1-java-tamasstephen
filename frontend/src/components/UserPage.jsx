229227e8c8d669ca0d2666aabe927ebf8210ccd4
matekaszas13 <matekaszas17@gmail.com>
2022-04-28 20:28:48 +0200

    Feat: Add list all user and add stlye

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -1,29 +1,34 @@
-import { data } from 'autoprefixer';
-import React, { useEffect, useState } from 'react'
-import { dataHandler } from '../data/dataHandler';
-import User from './model/User';
+import { data } from "autoprefixer";
+import React, { useEffect, useState } from "react";
+import { dataHandler } from "../data/dataHandler";
+import User from "./model/User";
+import Headline from "./Headline";
 
 const UserPage = () => {
-  const [users, setUsers] = useState({});
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
       return await dataHandler.getAllUser();
     };
     fetchUsers().then((data) => {
-      console.log(data);
-      setUsers(data);
-    })
+      setUsers(data.customers);
+    });
   }, []);
 
-
   return (
-    <div>
-      {users.map((user, index) => (
-        <User key={index} user={user}/>
-      ))}
+    <div className="px-4 md:px-0 md:flex md:flex-col md:items-center pt-8 md:pt-16">
+      <div className="md:w-8/12 md:max-w-3xl">
+        <Headline isTitle={true} title={"Users"}  />
+      <ul className="pt-4" >
+        {users.map((user, index) => (
+          <User key={index} user={user} />
+        ))}
+      </ul>
     </div>
-  )
-}
+    </div>
+    
+  );
+};
 
-export default UserPage
+export default UserPage;
