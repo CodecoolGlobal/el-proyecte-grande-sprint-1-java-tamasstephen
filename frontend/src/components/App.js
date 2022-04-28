b9588e8c248dbeeba29b3f460f59bb60ae8045af
matekaszas13 <matekaszas17@gmail.com>
2022-04-28 20:27:58 +0200

    Chore: Add setAdminState prop

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,12 +2,12 @@ import Navigation from "./Navigation";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
-function App({ userLogin, setLoginState, adminState }) {
+function App({ userLogin, setLoginState, adminState, setAdminState }) {
   return (
     <>
       <div className="flex flex-col min-h-screen justify-between bg-slate-100 overflow-hidden">
         <div>
-          <Navigation setLoginState={setLoginState} userLogin={userLogin} adminState={adminState} />
+          <Navigation setLoginState={setLoginState} userLogin={userLogin} adminState={adminState} setAdminState={setAdminState} />
           <Outlet />
         </div>
         <Footer />
